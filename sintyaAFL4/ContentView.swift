2295788c3e665fc73172647566fb0ed1e1f7c85a
armaidalstr <armaidalstr@gmail.com>
2022-06-03 19:11:15 +0700

    Initial Commit

---
+++ sintyaAFL4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sintyaAFL4
+//
+//  Created by MacBook Pro on 03/06/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sintyaAFL4/sintyaAFL4App.swift
@@ -0,0 +1,17 @@
+//
+//  sintyaAFL4App.swift
+//  sintyaAFL4
+//
+//  Created by MacBook Pro on 03/06/22.
+//
+
+import SwiftUI
+
+@main
+struct sintyaAFL4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
